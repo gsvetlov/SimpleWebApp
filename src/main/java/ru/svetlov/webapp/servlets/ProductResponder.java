d70b571d3a36be26d6920ca5f3a88d10caf1b37b
Georg Svetlov <gsvetlov@gmail.com>
2021-07-21 16:44:17 +0300

    вывод результатов запроса в jsp

--- src/main/java/ru/svetlov/webapp/servlets/ProductResponder.java
+++ src/main/java/ru/svetlov/webapp/servlets/ProductResponder.java
@@ -2,6 +2,7 @@ package ru.svetlov.webapp.servlets;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ru.svetlov.webapp.domain.Product;
 import ru.svetlov.webapp.factory.Factory;
 import ru.svetlov.webapp.service.ProductInfoService;
 
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet(name = "ProductPage", urlPatterns = "/products")
 public class ProductResponder extends HttpServlet {
@@ -19,11 +21,11 @@ public class ProductResponder extends HttpServlet {
     private ProductInfoService productInfo;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         log.info("Request from {}:{}", req.getRemoteAddr(), req.getRemotePort());
-        PrintWriter writer = resp.getWriter();
-        productInfo.getProducts(10).forEach(writer::println);
-        log.info("Responding with {}", resp.getStatus());
+        req.setAttribute("productList", productInfo.getProducts(10));
+        req.getRequestDispatcher("/products.jsp").forward(req, resp);
+        log.info("Forwarding with {}", resp.getStatus());
     }
 
     @Override
